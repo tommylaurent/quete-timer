53e021469e62b50d3e85377633b4f1c1276f80d0
Tommy Laurent <tommylaurent@protonmail.com>
2018-06-04 11:07:10 +0200

    creation d'une horloge

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Clock from "./Clock";
+//import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { date: new Date() };
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Clock />
       </div>
     );
   }
